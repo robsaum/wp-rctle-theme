f3b9fdc4aaca32d27c49a63abf9729b51941b3c3
robsaum <rob@boundlessliving.org>
2020-11-03 11:32:12 -0500

    Add taxonomies: tags and categories to pages/posts.

--- functions.php
+++ functions.php
@@ -94,4 +94,25 @@ function new_excerpt_more($more) {
   return '...<br><a class="btn btn-primary" role="button" href="'. get_permalink($post->ID) . '"> Read More...</a>';
 }
 add_filter('excerpt_more', 'new_excerpt_more');
+
+
+function add_taxonomies_to_pages() {
+ register_taxonomy_for_object_type( 'post_tag', 'page' );
+ register_taxonomy_for_object_type( 'category', 'page' );
+ }
+add_action( 'init', 'add_taxonomies_to_pages' );
+ if ( ! is_admin() ) {
+ add_action( 'pre_get_posts', 'category_and_tag_archives' );
+ 
+ }
+function category_and_tag_archives( $wp_query ) {
+$my_post_array = array('post','page');
+ 
+ if ( $wp_query->get( 'category_name' ) || $wp_query->get( 'cat' ) )
+ $wp_query->set( 'post_type', $my_post_array );
+ 
+ if ( $wp_query->get( 'tag' ) )
+ $wp_query->set( 'post_type', $my_post_array );
+}
+
 ?>
