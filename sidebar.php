260146deb9058159af3d6ccde70f5711921b2b3d
robsaum <rob@boundlessliving.org>
2020-11-10 18:21:57 -0500

    Add subcategories, description, and post count to sidebar.

--- sidebar.php
+++ sidebar.php
@@ -16,4 +16,41 @@
     </ul>
 <?php } ?>
 
-<?php // wp_tag_cloud('smallest=8&largest=22'); ?>
+
+
+
+<hr>
+
+<?php 
+
+// wp_tag_cloud('smallest=8&largest=22'); 
+
+// 1) Get the category array,
+$category = get_the_category(get_the_ID());
+
+// 2) Get the main category of the page
+$catid=$category[0]->category_parent;
+if($catid==0){
+  $catid=$category[0]->cat_ID;
+}
+
+// 3) Get the children categories
+$categories = get_categories('child_of='.intval($catid));            
+
+foreach ($categories as $category) {
+    // Validation
+    if ($category->parent ==$catid):
+        echo '<p><a href="' . get_category_link( $category->term_id ) . '">'.$category->cat_name.'</a> ('.$category->count.')<br />';
+        // Get the subcategory
+            echo $category->description . '</p>';
+
+        $subcategories=  get_categories('child_of='.intval($category->cat_ID));
+        /*
+        foreach ($subcategories as $subcategory) {
+            echo '<span class="subcategory" style="padding-left:15px">';
+            echo '<a href="">'.$subcategory->cat_name.'</a></span>';
+        } */
+    endif;
+}
+
+ ?>
